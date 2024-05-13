315ca865771b355c1eb5eecab7a5a05a757967ab
rebel47 <aayyaazz123@gmail.com>
2024-05-13 16:53:42 +0200

    lil changes

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def input_image_setup(uploaded_file):
 
 st.set_page_config(page_title="Gemini Health App")
 
-st.header("Gemini Health Application")
+st.header("Gemini Health App")
+st.title("Upload your meal image to get the calories detail")
 ## input=st.text_input("Input Prompt: ",key="input")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image=""   
